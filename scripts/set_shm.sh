5db566c20fec2eed6724a6953f268575b61006a0
zongdai <zongdai@ed5bfd34-b61d-4ad5-b956-f82ce90ca771>
2011-11-22 03:06:21 +0000

    modify set shm script
    git-svn-id: http://code.taobao.org/svn/tair/trunk@367 ed5bfd34-b61d-4ad5-b956-f82ce90ca771

--- scripts/set_shm.sh
+++ scripts/set_shm.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-size=22G
+size=23G
 if [ x$1 != x ]
 then
 	echo $1
 	size=$1
 fi
-sed -i "s;^\(none */dev/shm * tmpfs * defaults\).* \( * 0 0\);\1,size=${size}\2;g" /etc/fstab
+sed -i "/tmpfs/ s/defaults/rw,size=${size}/" /etc/fstab
 mount -o remount /dev/shm
