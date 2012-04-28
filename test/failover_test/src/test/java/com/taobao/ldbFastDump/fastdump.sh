4ad3df5e69dd675ec7869650cef67530ca7465a0
ashu <ashu@ed5bfd34-b61d-4ad5-b956-f82ce90ca771>
2012-04-28 03:53:06 +0000

    shell on cs
    git-svn-id: http://code.taobao.org/svn/tair/trunk@790 ed5bfd34-b61d-4ad5-b956-f82ce90ca771

---
+++ test/failover_test/src/test/java/com/taobao/ldbFastDump/fastdump.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+PORT=5168
+
+group=$1
+par2=$2
+shift
+while [ ! x$1 = x ]
+do
+    all="$all $1"
+    shift
+done
+./sbin/tairclient -c 10.232.4.14:${PORT} -g $group -l "$all" 2>out
+
+getCount()
+{
+    line=`cat out |wc -l`
+    if [ $line -eq 8 ]
+    then
+        echo 0
+        return 0
+    fi
+    cat out | grep getCount| tail -1|awk -F " " '{print $3}'
+}
+
+cmd()
+{
+    cat out
+}
+
+case $par2 in
+stat)
+    getCount
+    ;;
+*)
+    cmd
+    ;;
+esac
+exit 0
